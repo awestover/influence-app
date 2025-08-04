54fcf5db4ab5ae3307a780ed343f7de21d8f5181
Alek Westover <alek.westover@gmail.com>
2025-08-04 16:49:11 -0700

    update

--- 0.py
+++ 0.py
@@ -71,7 +71,7 @@ print("LOADING MODEL")
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.float16, device_map="auto")
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
-LR = 1e-3
+LR = 1e-5
 
 print("COMPUTING logprobs")
 print(get_logprobs(model, tokenizer, test_Q, test_A))
