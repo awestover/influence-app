e361b480c5138b6f586123c6b41f2eb5c815378c
Alek Westover <alek.westover@gmail.com>
2025-08-07 08:52:14 -0700

    update

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def get_logprobs(model, tokenizer, query_messages, response):
         # GET RID OF EOS
         response_logits = logits[0, query_len-1:-3]
         response_targets = full_ids[0, query_len:-2]
-        import ipdb; ipdb.set_trace()
         log_probs = F.log_softmax(response_logits, dim=-1)
         target_log_probs = log_probs.gather(1, response_targets.unsqueeze(1)).squeeze(1)
         return target_log_probs.sum().item()
